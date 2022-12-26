7e1a55a1fb1d6c677131746a48647437c0c5b2ef
Aleksander Mishutkin <106084948+AlexanderMishutkinP@users.noreply.github.com>
2022-12-26 18:55:47 +0100

    BTree Utils

--- utils/generic-types/pair.go
+++ utils/generic-types/pair.go
@@ -1,1 +1,12 @@
-package pair
+package generic_types
+
+import "fmt"
+
+type KeyVal[K, V any] struct {
+	Key   K
+	Value V
+}
+
+func (receiver KeyVal[K, V]) String() string {
+	return fmt.Sprint("{ ", receiver.Key, ": ", receiver.Value, " }")
+}
