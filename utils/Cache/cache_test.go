ac77fb0b96938c5185bfaa58a4883e634374900f
Balša Bulatović <63477276+BulatovicBalsa@users.noreply.github.com>
2023-01-24 23:25:28 +0100

    Add files via upload

---
+++ utils/Cache/cache_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+func SlicesEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestCache(t *testing.T) {
+	cache := New[int, []byte](5)
+	cache.Refer(5, []byte("balsa"))
+	cache.Refer(6, []byte("teodor"))
+	cache.Refer(7, []byte("vlada"))
+	cache.Refer(8, []byte("danilo"))
+
+	if !SlicesEqual(cache.Refer(7, nil), []byte("vlada")) {
+		t.Fatalf("Cache doesn't contain good value for key 7")
+	}
+
+	cache.Refer(9, []byte("sasa"))
+
+	if cache.Contains(15) {
+		t.Fatalf("Failed, key 15 should not exists")
+	}
+
+	for i := 5; i < 10; i++ {
+		if !cache.Contains(i) {
+			t.Fatalf("Cache failed for key " + strconv.Itoa(i))
+		}
+	}
+
+}
