33d72125abff2e98e8ae79b2a295092ffa0f3290
Vlada <vladapopov02@gmail.com>
2023-01-29 00:53:06 +0100

    reset main

--- main.go
+++ main.go
@@ -2,44 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
-	"nosql-engine/packages/utils/wal"
-	"os"
-	"time"
 )
-
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
 
 func main() {
 	fmt.Println("test")
-
-	rand.Seed(time.Now().UnixNano())
-	elementsCnt := 100
-	segmentSize := 20
-	path := "../../data/wal/"
-
-	wal := wal.New(path, uint32(segmentSize), 10)
-	randomStr := make([]string, elementsCnt)
-
-	for i := 0; i < elementsCnt; i++ {
-		randomStr[i] = randSeq(10)
-		wal.PutEntry(randomStr[i], []byte(randomStr[i]), 0)
-	}
-
-	for i := 0; i < int(math.Ceil(float64(elementsCnt)/float64(segmentSize))); i++ {
-		_, error := os.Stat(path + "log_" + fmt.Sprint(i) + ".bin")
-
-		if os.IsNotExist(error) {
-			fmt.Println("error nema ga")
-		}
-	}
 }
