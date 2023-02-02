22785588bc0464e358b542eb284328f270256f7d
Balša Bulatović <baki.b.2k02@gmail.com>
2023-02-02 18:51:12 +0100

    prefix scan

--- utils/sstable/sstable.go
+++ utils/sstable/sstable.go
@@ -650,6 +650,102 @@ func checkPrefixIndex(key string, filename string, start uint64, stop uint64) []
 	}
 }
 
+func RangeScan(key1, key2, prefix string, levels uint64, mode string) map[string]database_elem.DatabaseElem {
+	kvMap := make(map[string]database_elem.DatabaseElem)
+
+	if key1 > key2 {
+		return kvMap
+	}
+
+	filespath := prefix
+	arrToc := readOrder(prefix, levels)
+
+	for _, name := range arrToc {
+		fmap := readTOC(name, filespath, mode)
+		summOffset, _ := uint64(0), uint64(0)
+		if mode == "one" {
+			_, summOffset, _ = readFileOffsets(fmap["data"])
+		}
+
+		found, start, stop := checkRangeSummary(key1, key2, fmap["summary"], summOffset)
+		if !found {
+			continue
+		}
+		offsets := checkRangeIndex(key1, key2, fmap["index"], start, stop)
+		for _, start := range offsets {
+			deleted, dbel, key := readDataWithKey(fmap["data"], start)
+			if deleted {
+				continue
+			}
+			_, ok := kvMap[key]
+			if !ok {
+				kvMap[key] = dbel
+			}
+		}
+	}
+	return kvMap
+}
+
+func checkRangeSummary(key1, key2, filename string, fileOffset uint64) (bool, uint64, uint64) { //returns range of index bytes where key may be
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	file.Seek(int64(fileOffset), io.SeekStart)
+	start := readKey(*file)
+	stop := readKey(*file)
+	if start > key2 || stop < key1 {
+		return false, 0, 0
+	}
+	prevoffset := uint64(0)
+	firstIter := true
+	for {
+		filekey := readKey(*file)
+		offset := readUint64(*file)
+		if firstIter {
+			firstIter = false
+			prevoffset = offset
+		}
+		if filekey < key1 {
+			prevoffset = offset
+		}
+		if filekey > key2 || stop == filekey {
+			return true, prevoffset, offset
+		}
+	}
+}
+
+func checkRangeIndex(key1, key2, filename string, start uint64, stop uint64) []uint64 {
+	arr := make([]uint64, 0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	file.Seek(int64(start), io.SeekStart)
+	for {
+		pos, _ := file.Seek(0, io.SeekCurrent)
+		if stop < uint64(pos) {
+			return arr
+		}
+		filekey := readKey(*file)
+		offset := readUint64(*file)
+		if isInRange(filekey, key1, key2) {
+			arr = append(arr, offset)
+		}
+	}
+}
+
+func isInRange(key, key1, key2 string) bool {
+	if key1 <= key && key <= key2 {
+		return true
+	}
+	return false
+}
+
 func ReadFileOffset(filename string) uint64 { //index
 	readFile, err := os.Open(filename)
 	if err != nil {
